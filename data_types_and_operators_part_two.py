25e92b70b389f22ba780a52d170b227ffeeef8e2
Alec Hartman <aleclhartman08@gmail.com>
2020-02-19 16:01:10 -0600

    committing work for the day

---
+++ data_types_and_operators_part_two.py
@@ -0,0 +1,89 @@
+# IPython log file
+
+
+l = [1, 2, 3]
+len(l)
+l.append([4])
+l
+l.pop([4])
+l.pop()
+l
+l + [4]
+l
+l[1:]
+l
+l.append(4)
+l
+print(l)
+l.pop()
+l
+l.pop()
+l
+l.append(2, 3, 4)
+l
+l.append(3)
+l
+l.append(900)
+l.append(16)
+l.append(23)
+l
+l + 42
+l + [42]
+new_list = print(l + [56])
+new_list
+range(10)
+type(range)
+type(range())
+type(range(10))
+list(range(10))
+list('abc')
+list(14.9)
+(1, 2, 3)
+type((1, 2, 3))
+my_tuple = (1, 2, 3)
+[(10001, 'alec', 'hartman', 2020-02-19), ()]
+[(10001, 'alec', 'hartman', '2020-02-19'), ()]
+type([(10001, 'alec', 'hartman', '2020-02-19'), ()])
+mean = {'type': 'breakfast', 'contents': 'yogurt and berries', 'beverage': 'water'}
+mean
+meal = {'type': 'breakfast', 'contents': 'yogurt and berries', 'beverage': 'water'}
+meal
+13
+x
+x = 13
+x
+x
+meal['contents']
+meal['price'] = 10
+meal
+meal['price'] *= 1.2
+meal
+x
+x += 3
+x
+x -= 6
+x
+x /= 5
+x
+x *= 5
+x
+x //= 2
+x
+x *= 2
+z
+x
+x = 15
+x
+x //= 3
+x
+dict(x=123, y=456, z=789)
+round(4.7)
+round(4.4)
+round(4.6)
+round(4.5)
+dict(x=123, y=456, z=789)
+di = dict{x=123, y=456, z=789}
+d1 = dict(x=123, y=456, z=789)
+d2 = {'x': 123, 'y': 456, 'z': 789}
+d1 == d2
+exit()
